f2f0bdc53a22e576492e9549fb804e90d758d2c8
Praveen Nair <nairpraveen1997@gmail.com>
2023-07-26 18:50:14 +0530

    Created a Stock trade Statistics using ReactJS & ExpressJS.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,93 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
 function App() {
-	return (
-		<h1>Hello world</h1>
-	);
+  const [symbol, setSymbol] = useState("");
+  const [date, setDate] = useState("");
+  const [tradeData, setTradeData] = useState(null);
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setError("");
+      setTradeData(null);
+
+      const response = await axios.post(
+        "http://localhost:5000/api/fetchStockData",
+        { symbol, date }
+      );
+
+      // Update the tradeData state with the received data
+      setTradeData(response.data);
+    } catch (error) {
+      console.log(error);
+      setError("Error fetching stock data. Please check the symbol and date.");
+      console.error("Error fetching stock data:", error.message);
+    }
+  };
+
+  return (
+    <div className="app">
+      <h1 className="title">Stock Trade Statistics</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <table className="table">
+          <tr className="tableRow">
+            <td>
+              <label htmlFor="symbol">Stock Symbol:</label>
+            </td>
+            <td>
+              <input
+                type="text"
+                id="symbol"
+                value={symbol}
+                onChange={(e) => setSymbol(e.target.value)}
+                required
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label htmlFor="date">Date:</label>
+            </td>
+            <td>
+              <input
+                type="date"
+                id="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                required
+              />
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="2">
+              <button type="submit" className="btn-submit">
+                Submit
+              </button>
+            </td>
+          </tr>
+        </table>
+      </form>
+
+      {tradeData && (
+        <div className="trade-data">
+          <h2>
+            Trade Statistics for {symbol} on {date}
+          </h2>
+          <p>Open: {tradeData.open}</p>
+          <p>High: {tradeData.high}</p>
+          <p>Low: {tradeData.low}</p>
+          <p>Close: {tradeData.close}</p>
+          <p>Volume: {tradeData.volume}</p>
+        </div>
+      )}
+
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
 }
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -1,34 +1,61 @@
 // DO NOT MODIFY ANYTHING HERE, THE PLACE WHERE YOU NEED TO WRITE CODE IS MARKED CLEARLY BELOW
 
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 
 const app = express();
 
 app.use(function (req, res, next) {
-    const allowedOrigins = ['http://localhost:3000'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-    next();
+  const allowedOrigins = ["http://localhost:3000"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
 });
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
-app.enable('trust proxy');
+app.enable("trust proxy");
 
-app.post('/api/fetchStockData', (req, res) => {
-    // YOUR CODE GOES HERE, PLEASE DO NOT EDIT ANYTHING OUTSIDE THIS FUNCTION
-    res.sendStatus(200);
+app.post("/api/fetchStockData", async (req, res) => {
+  // YOUR CODE GOES HERE, PLEASE DO NOT EDIT ANYTHING OUTSIDE THIS FUNCTION
+  try {
+    const { symbol, date } = req.body;
+
+    // Validate symbol and date inputs
+    if (!symbol || !date) {
+      return res.status(400).json({ error: "Symbol and date are required." });
+    }
+
+    // Fetch trade data from Polygon API
+    const response = await axios.get(
+      `https://api.polygon.io/v1/open-close/${symbol}/${date}?adjusted=true&apiKey=eUpsaIhAYostOhbwuvQ6_cTeHTJ9XyEM`
+    );
+
+    const { open, high, low, close, volume } = response.data;
+
+    // Return only the required fields
+    return res.json({ open, high, low, close, volume });
+  } catch (error) {
+    console.error("Error fetching stock data:", error.message);
+    return res
+      .status(500)
+      .json({ error: "Something went wrong while fetching stock data." });
+  }
 });
 
 const port = process.env.PORT || 5000;
